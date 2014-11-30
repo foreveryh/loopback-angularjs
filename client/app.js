b11a91212e05d4c15927f63acdb354a004da85e6
foreveryh <foreveryh@gmail.com>
2014-11-30 17:44:42 +0900

    Add angularjs seed

---
+++ client/app.js
@@ -0,0 +1,23 @@
+angular.module('Shu', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('Shu').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('Shu').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
