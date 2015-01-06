78f735858979030a234ac648e5951335d4651910
foreveryh <foreveryh@gmail.com>
2015-01-06 23:04:05 +0900

    Angualr:ocLazyload added

--- client/states/write/write.js
+++ client/states/write/write.js
@@ -1,11 +1,20 @@
-angular.module('Shu.write', ['ui.bootstrap','ui.utils','ui.router','ngAnimate','textAngular']);
+angular.module('Shu.write', ['ui.bootstrap','ui.utils','ui.router','ngAnimate','oc.lazyLoad']);
 
 angular.module('Shu.write').config(function($stateProvider) {
 
 	$stateProvider.state('write', {
  		url: '/write',
-    templateUrl: 'states/write/write.html'
+ 		templateUrl: 'states/write/write.html',
+ 		resolve:{  
+ 			// Any property in resolve should return a promise and is executed before the view is loaded
+			 loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+         // you can lazy load files for an existing module
+         return $ocLazyLoad.load({
+            name: 'textAngular',
+            files: ['http://cdn.jsdelivr.net/g/angular.textangular@1.2.2(textAngular-sanitize.min.js+textAngular.min.js)']
+         });
+      }]
+    }
   });
-
 });
 
