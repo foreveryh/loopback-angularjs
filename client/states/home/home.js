af7a7aa835c00843634fae2552c466e6f7f1fcac
foreveryh <foreveryh@gmail.com>
2015-04-02 14:00:51 +0900

    Home: Load more funciton added

--- client/states/home/home.js
+++ client/states/home/home.js
@@ -19,32 +19,56 @@ angular.module('Shu.home').config(function($stateProvider) {
       });
   })
   .controller('HomeCtrl', function($rootScope, $scope, $window, Article) {
+    var defaultState = false; // true - recommend, false - hottest
+    var recommendItems = Article.find({
+        filter: {
+          limit: 2
+        }
+      },
+      function(success) {},
+      function(error) {});
+
+    var hottestItems = Article.find({
+        filter: {
+          limit: 2
+        }
+      },
+      function(list) {
+        /* success */
+      },
+      function(error) {
+        /* error */
+      });
 
     $scope.$on('$stateChangeSuccess', function(evt, toState, toParams, fromState, fromParams) {
       $window.document.title = '首页 - 短篇';
       if (fromState.name !== 'recommend' && fromState.name !== 'hottest') {
         $rootScope.bodylayout = "output reader-day-mode reader-font2";
       }
-      if (toState.name === 'recommend') {
-        $scope.articles = Article.find({
-            filter: {
-              limit: 5
-            }
-          },
-          function(success) {},
-          function(errorResponse) {});
-      } else {
-        $scope.articles = Article.find({
-            filter: {
-              limit: 10
-            }
-          },
-          function(list) {
-            /* success */
-          },
-          function(errorResponse) {
-            /* error */
-          });
-      }
+      defaultState = toState !== "hottest" ? true : false;
+      $scope.articles =  defaultState ? recommendItems: hottestItems;
+ 
     });
+    $scope.loadMore = function() {
+
+      var skipLen = defaultState ? recommendItems.length : hottestItems.length;
+      Article.find({
+        filter: {
+          limit: 2,
+          skip: skipLen
+        }
+      },
+      function(success){
+        if (defaultState) {
+          Array.prototype.push.apply(recommendItems, success);
+          console.log("Load more successly");
+        }else{
+          Array.prototype.push.apply(hottestItems,success);
+        }
+      },
+      function(error){
+        console.log(error);
+      });
+    };
+
   });
